e1f49387cabbb273a55000efedffad86fcdc76bb
Johan Janérs <johanjaners@gmail.com>
2025-10-03 16:38:51 +0200

    App.jsx: Updating screen: count added to MyButton

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import './App.css'
 import { useState } from 'react';
 
 function MyButton() {
-  function handleClick() {alert('You clicked me!');}
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
-    <button onClick={handleClick}>Click me</button>
+    <button onClick={handleClick}>
+      Clicked {count} times
+      </button>
   );
 }
 function AboutPage() {
